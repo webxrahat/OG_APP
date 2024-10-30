557af099484051a72377953328bba64d1bcc0d2a
webxrahat <islammdrahat308@gmail.com>
2024-10-30 18:01:48 +0600

    rr

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/keto', function(){
 Route::get('/post/firstpost', function(){
     return view('firstpost');
 });
+
+Route::get('/post/{id}', function(string $id){
+    return "<h1>This is values ". $id."  </h1>";
+});
+
+Route::get('/{id}', function($if){
+    return "<p>This is new line of values ". $if ." </p>";
+});
