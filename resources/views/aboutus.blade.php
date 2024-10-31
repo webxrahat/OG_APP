c192d96dbf193800fdae37fced26fddb900f5584
webxrahat <islammdrahat308@gmail.com>
2024-10-31 13:13:46 +0600

    rr

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -5,5 +5,17 @@
     </h1>
 
         
-        
-        
+@php
+    $name = ["san", "man", "pan"];
+    $age = 21;
+@endphp
+        {{$age}}
+
+       <ul>
+         @foreach ($name as $n)
+
+         <li>{{$n}}</li>
+             
+         @endforeach
+       </ul>
+      
